e0a0f7159670819ee374ab8e199689a2c80a155b
Sabina Daria <sabina.daria3@yahoo.com>
2021-04-05 21:36:44 +0300

    sec test

--- src/test/java/test/java/StudentTest.java
+++ src/test/java/test/java/StudentTest.java
@@ -42,5 +42,34 @@ public class StudentTest {
         assert size1 +1 == size2;
         service.deleteStudent("uniqueId");
     }
+    @Test
+    void addStudentTest2(){
+        Validator<Student> studentValidator = new StudentValidator();
+        Validator<Tema> temaValidator = new TemaValidator();
+        Validator<Nota> notaValidator = new NotaValidator();
+
+        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
+        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
+        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
+
+        Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
+        AtomicInteger students = new AtomicInteger();
+        AtomicInteger students2 = new AtomicInteger();
+        AtomicInteger students3 = new AtomicInteger();
+        service.findAllStudents().forEach(stud->{
+            students.addAndGet(1);});
+        service.saveStudent("uniqueId","Daria",932);
+        service.findAllStudents().forEach(stud->{
+            students2.addAndGet(1);});
+        int size1 = students.get();
+        int size2 = students2.get();
+        assert size1 +1 == size2;
+        service.saveStudent("uniqueId","Daria",932);
+        service.findAllStudents().forEach(stud->{
+            students3.addAndGet(1);});
+        int size3 = students3.get();
+        assert size3  ==size2;
+        service.deleteStudent("uniqueId");
+    }
 
 }
